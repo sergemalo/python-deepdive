860abe3e5d854942e16f0eeb6acbbd09c9551c6a
Serge Malo <sergemalo2000@outlook.com>
2023-01-07 11:14:43 -0500

    Notes up to for loops

--- perso/example.py
+++ perso/example.py
@@ -1,7 +1,24 @@
 # import this
-def my_func():
-    print("""A
-    A""")
+def my_func(a: int, b: int):
+    print(
+        """A
+    A"""
+    )
+    return a * b
 
 
-my_func()
+#my_func(2, "aaa")
+
+def my_loop():
+    i = 5
+    while True:
+        print(i)
+        if i >= 5:
+            break
+
+
+sq = lambda x: x**2
+
+def print_str(the_str):
+    for i, c in enumerate(the_str):
+        print (i, c)
